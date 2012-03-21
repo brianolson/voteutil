b25b11bfc8ec72f7a78c3011b5ee1a167d6aba96
Brian Olson <brian.olson@gmail.com>
2012-03-21 17:43:13 -0400

    t
    --HG--
    branch : brian

--- go/lib/vrr_test.go
+++ go/lib/vrr_test.go
@@ -9,3 +9,11 @@ func TestVrrBasic(t *testing.T) {
 func TestVrrTie(t *testing.T) {
 	RankTie(t, new(VRR))
 }
+
+func TestVrr2(t *testing.T) {
+	var x NameVote
+	x = make(NameVote, 2)
+	em := new(VRR)
+	em.Vote(x)
+	_, _ = em.GetResult()
+}
