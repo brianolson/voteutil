d7f345c17c19b187bf744338fdb087b7488aa6c0
Brian Olson <brian.olson@gmail.com>
2020-07-23 08:32:59 -0400

    utility to unpack xlsx data into csv

---
+++ python/xlsxtocsv.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+#
+# pip install openpyxl
+
+import csv
+import logging
+
+import openpyxl
+
+logger = logging.getLogger(__name__)
+
+def wb_sheet_out(wb, sheetname, outname):
+    sheet = wb[sheetname]
+    with open(outname, 'wt') as fout:
+        writer = csv.writer(fout)
+        for row in sheet.iter_rows(values_only=True):
+            writer.writerow(row)
+
+
+def xlsxtocsv(pathin, outdir=''):
+    wb = openpyxl.load_workbook(pathin)
+    if pathin.endswith('.xlsx'):
+        rootname = pathin[:-5]
+    elif pathin.endswith('.xls'):
+        rootname = pathin[:-4]
+    else:
+        rootname = pathin
+    if outdir:
+        outroot = os.path.join(outdir, os.path.basename(rootname))
+    else:
+        outroot = rootname
+    if len(wb.sheetnames) == 1:
+        outname = outroot + '.csv'
+        logger.info('%s -> %s', pathin, outname)
+        wb_sheet_out(wb, wb.sheetnames[0], outname)
+        return
+    for sn in wb.sheetnames:
+        outname = outroot + '_' + sn + '.csv'
+        logger.info('%s[%s] -> %s', pathin, sn, outname)
+        wb_sheet_out(wb, sn, outname)
+
+
+def main():
+    import argparse
+    ap = argparse.ArgumentParser()
+    ap.add_argument('xlsxfiles', nargs='*')
+    ap.add_argument('--verbose', default=False, action='store_true')
+    args = ap.parse_args()
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.WARNING)
+
+    for path in args.xlsxfiles:
+        xlsxtocsv(path)
+
+if __name__ == '__main__':
+    main()
